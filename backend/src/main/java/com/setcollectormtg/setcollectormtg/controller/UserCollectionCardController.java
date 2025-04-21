dada990818708b4f755e7e19cacc7171267adc20
Brais Becerra Bermúdez <brais.becerra@altia.es>
2025-04-21 16:52:26 +0200

    Imports

--- backend/src/main/java/com/setcollectormtg/setcollectormtg/controller/UserCollectionCardController.java
+++ backend/src/main/java/com/setcollectormtg/setcollectormtg/controller/UserCollectionCardController.java
@@ -4,17 +4,23 @@ import com.setcollectormtg.setcollectormtg.dto.UserCollectionCardDto;
 import com.setcollectormtg.setcollectormtg.service.UserCollectionCardService;
 import com.setcollectormtg.setcollectormtg.service.UserCollectionService;
 import com.setcollectormtg.setcollectormtg.service.UserService;
-import com.setcollectormtg.setcollectormtg.repository.UserRepository;
 import com.setcollectormtg.setcollectormtg.model.UserCollection;
 import com.setcollectormtg.setcollectormtg.model.User;
-import com.setcollectormtg.setcollectormtg.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/collection/cards")
